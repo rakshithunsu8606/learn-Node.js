91502e6ad7cc90595fc9b2b26ff5e97b1ed2bb95
Rakshit Hinsu <rakshithinsu8606@gmail.com>
2026-03-24 17:07:28 +0530

    24/3/26

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cookieParser = require('cookie-parser')
 const routes = require("./routes/api/v1/index.js");
 const ConnectionMongoDB = require('./db/MongoDB.js');
-const {googleProvider,facebookProvider} = require('../server/passport.js');
+const { googleProvider, facebookProvider } = require('../server/passport.js');
 // const facebookProvider = require('../server/passport.js');
 const passport = require('passport');
 const createSocket = require('../server/createSocket.js');
@@ -37,6 +37,12 @@ console.log(process.env.PORT)
 //http://localhost:8080/api/v1
 app.use('/api/v1', routes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Started At ${process.env.PORT}`);
+app.get('/', (req, res) => {
+  res.send('Welcome The LMS Backend')
 })
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server Started At ${process.env.PORT}`);
+// })
+
+module.exports = app;
